a3c290db290be4fb82964f50b70d1934183fab11
we-are-the-future33 <caramelsuit@gmail.com>
2026-03-02 12:34:29 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase, ref, get, set, remove, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-const APP_VERSION = '20260304f';
+const APP_VERSION = '20260304g';
 
 const _safetyTimer = setTimeout(() => {
   const l = document.getElementById('loadingScreen');

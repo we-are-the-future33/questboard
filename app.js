d702ca9de815930b49b02569af279e6a5c0e936c
we-are-the-future33 <caramelsuit@gmail.com>
2026-06-11 11:34:26 +0900

    Shrink debug box (20260611h)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getDatabase, ref, get, set, remove, push } from "https://www.gstatic.co
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const APP_VERSION = '20260611g';
+const APP_VERSION = '20260611h';
 
 const _safetyTimer = setTimeout(() => {
   const l = $id('loadingScreen');
